4f661aafdbd313e0dfdedaab75c3e0b177df7a7f
iskalievayan <iskaliev.ayan@icloud.com>
2026-02-06 13:16:43 +0500

    add bool_comparison.py to Practice2

---
+++ Practice2/bool/bool_comparison.py
@@ -0,0 +1,20 @@
+a=2
+b=4
+print(a>b) #False or 0
+print(a<b) #True or 1
+print(a==b) #also False bc they no equal
+
+#game guess number
+p=int(input())
+r=5
+print(p==r)
+
+#with strings
+s="a"
+s2="x"
+print(s>s2) #True
+print(s<s2) #False
+
+x=int(input())
+y=int(input())
+print(x==8,y==8)
