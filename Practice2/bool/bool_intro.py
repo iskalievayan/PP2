af02db36b32915632d3a6f522393a79040e0323c
iskalievayan <iskaliev.ayan@icloud.com>
2026-02-06 13:26:09 +0500

    add bool_intro to bool examples

---
+++ Practice2/bool/bool_intro.py
@@ -0,0 +1,10 @@
+b=True #it's bool type
+b2=False
+print(b, b2, b and b2, b or b2, not b, not b2, sep=" ") #logic operations
+
+#example how to use bool
+bl=input("Whats 1+1?")
+print(bl==2)
+
+a=True
+a=False
