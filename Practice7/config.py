6670b91d09f72c98ba4960e190b485ba22e6fedf
iskalievayan <iskaliev.ayan@icloud.com>
2026-04-28 18:36:49 +0500

    kk

--- Practice7/config.py
+++ Practice7/config.py
@@ -1,9 +1,5 @@
-def load_config():
-    """Returns the database connection configuration."""
-    return {
-        "dbname": "phonebook_db", # Make sure to create this database in pgAdmin or psql first
-        "user": "postgres",       # Your PostgreSQL username
-        "password": "password",   # Your PostgreSQL password
-        "host": "localhost",
-        "port": "5432"
-    }
+DB_NAME = "phonebook_db"
+DB_USER = "postgres"
+DB_PASSWORD = "1234"
+DB_HOST = "localhost"
+DB_PORT = "5432"
--- Practice7/connect.py
+++ Practice7/connect.py
@@ -1,13 +1,11 @@
 import psycopg2
-from config import load_config
+from config import *
 
-def connect():
-    """Connect to the PostgreSQL database server."""
-    try:
-        params = load_config()
-        print("Connecting to the PostgreSQL database...")
-        conn = psycopg2.connect(**params)
-        return conn
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(f"Connection error: {error}")
-        return None
+def get_conn():
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
